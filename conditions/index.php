e25ea122108acca3e3aa10f67927f61c74b4dee1
raul-markus.kivisalu <raul-markus.kivisalu@khk.ee>
2018-12-14 13:09:02 +0200

    switch käsk

--- conditions/index.php
+++ conditions/index.php
@@ -23,7 +23,7 @@ if ($arv % 2 == 0) {
 else {
     echo '<div style="color: red">'.$arv.'</div>';
 }
-
+echo '<hr>';
 echo '<h5ülesanne2</h5>';
 
 $arv2 = rand(0, 100);
@@ -39,3 +39,46 @@ elseif ($arv2 > 50 and $arv2 <= 75) {
 else {
     echo '<div style="color: red">'.$arv2.'</div>';
 }
+
+//switch (kontroll)
+//case:vastus
+//tegevused mis case-i puhul toimuvad
+//break;
+//default;
+//kõik tegevused mis toimivad kui muu ei sovi
+//break;
+?>
+<html>
+<style>
+    div {
+        width: 100px;
+        height: 100px;
+        border-radius: 50px;
+        border: solid 2px black;
+    }
+</style>
+</html>
+
+<?php
+echo '<hr>';
+echo '<h5ülesanne3</h5>';
+$aktiivne='punane';
+switch ($aktiivne) {
+    case 'punane';
+    echo '<div style="background: red;"></div>';
+    echo '<div></div>';
+    echo '<div></div>';
+}
+switch ($aktiivne) {
+    case 'kollane';
+    echo '<div></div>';
+    echo '<div style="background: yellow;"></div>';
+    echo '<div></div>';
+}
+switch ($aktiivne) {
+    case 'roheline';
+    echo '<div></div>';
+    echo '<div></div>';
+    echo '<div style="background: green;"></div>';
+}
+?>
