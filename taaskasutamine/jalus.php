ff169b1b7fe24cd263f04c99331503ca020d6c55
rkivi <raul-markus.kivisalu@khk.ee>
2019-03-27 11:51:06 +0200

    Avaleht + erinevad lehe failid valmist. Include jalus ja päis avalehele.

---
+++ taaskasutamine/jalus.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+    <title>Koodi taaskasutamine</title>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            font-family: Verdana, Geneva, sans-serif;
+        }
+        #pais {
+            background-color: #6C9;
+            line-height: 100px;
+        }
+        #jalus {
+            background-color: #F93;
+            line-height: 30px;
+        }
+    </style>
+</head>
+<body>
+<footer id="jalus">
+    <p>No kopirait</p>
+</footer>
+</body>
+</html>+
---
+++ taaskasutamine/portfoolio.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rkivi
+ * Date: 27/03/2019
+ * Time: 11:46
+ */
